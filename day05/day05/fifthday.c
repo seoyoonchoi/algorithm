c332735f330295b9cf1f40de7204cf27312ae0f2
seoyoonchoi <chsu0904@naver.com>
2025-03-19 19:37:23 +0900

    Start

---
+++ day05/day05/fifthday.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+	int age = 10;
+	float height = 122.2F;
+	double weight = 66.2;
+	char* name = "최서윤";
+	printf("나이 : %d 살\n", age);
+	printf("키 : %.1fcm\n", weight);
+	 
+	}
---
+++ printTest/printTest/printTest.c
@@ -0,0 +1,4 @@
+#include<stdio.h>//외부 또는 내부 파일을 현재 파일에 포함시킬 수 있는 명령어
+void main(){
+	printf("최서윤");//컴파일 단축기는 ctrl+F5
+}
